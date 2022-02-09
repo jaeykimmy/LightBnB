f798393bcf67b93b7a5e94e7b2be0a99e79bdf07
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-02-09 17:53:26 -0500

    lightbnb insert completed

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO users (name, email, password)
+VALUES ('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Louisa Meyer', 'jacksonrose@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Dominic Parks', 'victoriablackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+VALUES (1, 'Beeg House', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 1000, 3, 2, 3, 'Canada', 'Big Street', 'Toronto', 'Ontario', 'L8R9N2', 'true'),
+(2, 'Mid House', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 750, 2, 4, 5, 'Canada', 'Mid Street', 'Toronto', 'Ontario', 'L8R9N7', 'true'),
+(3, 'Smol House', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 600, 2, 7, 10, 'Canada', 'Smol Street', 'Toronto', 'Ontario', 'F5Y9N7', 'true');
+
+INSERT INTO reservations (guest_id, property_id, start_date, end_date)
+VALUES (1, 1, '2018-09-11', '2018-09-26'),
+(2, 2, '2019-01-04', '2019-02-01'),
+(3, 3, '2021-10-01', '2021-10-14');
+
+
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
+VALUES (3, 2, 3, 10, 'messages'),
+(2, 3, 2, 9, 'messages'),
+(2, 2, 3, 8, 'messages');
